c41fe1fc241ab9a03714ceba75bfff46860d8951
Gaëtan <gaetan.hrlt@gmail.com>
2023-03-25 18:58:39 +0100

    Added presence changer cmd

--- slash_commands/admin.py
+++ slash_commands/admin.py
@@ -28,7 +28,6 @@ class Admin(app_commands.Group):
 
         await interaction.response.send_message(files=files, ephemeral=True)
 
-
     @app_commands.command(name="sync", description="Sync bot app commands")
     async def _sync(
         self, interaction: discord.Interaction
@@ -43,5 +42,70 @@ class Admin(app_commands.Group):
             msg = "Successfully synced %i commands!" % len(synced)
 
         await interaction.followup.send(content=msg,ephemeral=True)
+
+    @app_commands.command(name="set_status", description="Set bot activity status")
+    @app_commands.choices(
+        status=[
+            Choice(name="Online", value='online'),
+            Choice(name="Idle", value='idle'),
+            Choice(name="Do not disturb", value='dnd'),
+            Choice(name="Invisible", value='invisible'),
+            Choice(name="Offline", value='offline')
+        ],
+        activity_type=[
+            Choice(name="Playing", value=0),
+            Choice(name="Streaming", value=1),
+            Choice(name="Listening", value=2),
+            Choice(name="Watching", value=3),
+            Choice(name="Custom", value=4),
+            Choice(name="Competing", value=5)
+        ]
+    )
+    async def _setStatus(
+        self, interaction: discord.Interaction, status: Choice[str],
+        activity_type: Choice[int]=None, activity: str=None, url: str=None
+    ) -> None:
+        await interaction.response.defer(ephemeral=True)
+
+        if activity_type is None and activity is None:
+            currentActivity = interaction.guild.me.activity
+            await bot.change_presence(status=status.value, activity=currentActivity)
+
+            msg = "Successfully updated status!"
+
+        else:
+            if activity_type is None:
+                activity_type = discord.ActivityType.playing
+            
+            if activity is None:
+                msg = "Please provide an activity!"
+
+            else:
+                if activity_type.value == discord.ActivityType.streaming and url is None:
+                    msg = "Please provide a stream URL!"
+
+                else:
+                    if activity_type.value == discord.ActivityType.streaming:
+                        if not url.startswith("https://"):
+                            url = "https://%s" % url
+                        
+                        activity = discord.Streaming(
+                            name=activity,
+                            url=url
+                        )
+
+                    else:
+                        activity = discord.Activity(
+                            name=activity,
+                            type=activity_type.value,
+                            url=url
+                        )
+            
+                    await bot.change_presence(status=status.value, activity=activity)
+                    msg = "Successfully updated status and activity!"
+
+        await interaction.followup.send(content=msg, ephemeral=True)
+
+
 
 slash.add_command(Admin(name="admin", description="All bot administration slash commands"))
